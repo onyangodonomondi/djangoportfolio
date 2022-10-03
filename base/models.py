ce2b63a8f119bffceae0f563a217c33f7cf29772
onyangodonomondi <onyangodonomondi@gmail.com>
2022-10-03 11:53:29 +0300

    overhaul

--- base/models.py
+++ base/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Resume(models.Model):
+    Name = models.CharField(max_length=50)
+    Age = models.IntegerField()
+    Email = models.EmailField()
+    Phone = models.CharField(max_length=15)
+    profilepic = models.ImageField(upload_to='profilepic', blank=True)
+    about = models.TextField(blank=True)
+    titles = models.CharField(max_length=50, blank=True)
+    intro = models.TextField(blank=True)
+    portfolio = models.ImageField(upload_to='portfolio', blank=True)
+
+
+    def __str__(self):
+        return self.Name
